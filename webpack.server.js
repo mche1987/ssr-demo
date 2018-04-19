874799ea16343a300fa31c1b214cd5c834bd94ee
mike <michaelkcheong@gmail.com>
2018-04-18 22:57:09 -0700

    Use webpacknodeexternals to slim bundle size

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
-const baseConfig = require("./webpack.base.js")
+const baseConfig = require("./webpack.base.js");
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     // inform webpack we're building fo nodeJS, not browser
@@ -13,7 +14,10 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+
+    // do not run any library in server side bundle (to not make server side bundle so large)
+    externals: [webpackNodeExternals()]
 }
 
 module.exports = merge(baseConfig, config);
