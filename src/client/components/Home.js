52e542b854489256f56a934c1fa37719700a3c71
mike <michaelkcheong@gmail.com>
2018-04-18 22:23:16 -0700

    That event handler won't work since only HTML gets sent to browser, not any js

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>I am the home component</div>
+    return (
+        <div>
+            <div>I am the home component</div>
+            <button onClick={() => console.log("HI THIS WON'T WORK")} >Press me!</button>
+
+        </div>
+    )
 }
 
 export default Home;
