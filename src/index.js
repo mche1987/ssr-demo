8fc0a142a1ff463bf81a36a67d6a8481fb472b34
mike <michaelkcheong@gmail.com>
2018-04-18 21:59:34 -0700

    Fix path bug in index for home component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const React = require('react');
 const renderToString = require("react-dom/server").renderToString;
-const Home = require('./client/components/Home').default ;
+const Home = require('./components/Home').default ;
 const app = express();
 
 app.get('/', (req, res) => {
