2162bda77f424edd19f2153dbcf9faac009d52e5
mike <michaelkcheong@gmail.com>
2018-04-19 23:51:08 -0700

    Update routes to use react-router-config

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react';
 import Home from './components/Home';
 import UsersList from './components/UsersList';
 
-export default () => {
-    return (
-        <div>
-            <Route exact path="/" component={Home} />
-            <Route path="/users" component={UsersList} />
-        </div>
-    )
-
-};
+export default [
+    {
+        path: '/',
+        component: Home,
+        exact: true
+    },
+    {
+        path: '/users',
+        component: UsersList,
+    },
+]
