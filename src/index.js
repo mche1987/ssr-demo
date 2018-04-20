3f08de3b60870a833ab6f7b29aeff0af3a2b322e
mike <michaelkcheong@gmail.com>
2018-04-19 23:22:45 -0700

    Wiring up redux for Server Side rendering

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
+
 const app = express();
 
 app.use(express.static('public')); // app contains public files
 app.get('*', (req, res) => {
-    res.send(renderer(req));
+    const store = createStore();
+
+    // Some logic to initialize
+    // and load data into store
+
+    res.send(renderer(req, store));
 })
 
 app.listen(3000, () => {
