29bfad7c8cafcedbd9e86148cdd9b2128dfe8b1e
Mike Cheong <michaelkcheong@gmail.com>
2018-04-22 10:41:13 -0700

    Fix mistake in App.js for render routes and test to make sure App appears for all routes

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import renderRoutes from 'react-router-config'
+import { renderRoutes } from 'react-router-config'
 
 const App = ({ route }) => {
-    return <div>{ renderRoutes(route.routes) }</div>
+    return (
+        <div>
+            <h1>I'm a header</h1>
+            <div>{renderRoutes(route.routes)}</div>
+        </div>    
+    );
 }
 
 export default {
