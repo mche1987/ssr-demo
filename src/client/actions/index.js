85688ee865031d7ce3d31b10f0eecf8efe7f7d21
Mike Cheong <michaelkcheong@gmail.com>
2018-04-22 10:04:02 -0700

    Previous change for client side code clean up unnecessary imports

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 export const FETCH_USERS = 'fetchuser'
 export const fetchUsers = () => async (dispatch, getState, axiosApi) => { // api passed in via applyMiddleware
     const res = await axiosApi.get("/users");
