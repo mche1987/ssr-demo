a34a283e69c6e2b077b3b92f74474b3ffef9c04a
Mike Cheong <michaelkcheong@gmail.com>
2018-04-22 09:45:22 -0700

    Set up auth proxy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import express from 'express';
+import proxy from 'express-http-proxy';
 import { matchRoutes } from 'react-router-config';
 import Routes from './client/Routes';
 import renderer from './helpers/renderer';
@@ -7,6 +8,13 @@ import createStore from './helpers/createStore';
 
 const app = express();
 
+app.use('/api', proxy('http://react-ssr-api.heroukuapp.com', {
+    // course specific configs below
+    proxyReqOptDecorator(opts){
+        opts.header['x-forwarded-host'] = 'localhost:3000';
+        return opts;
+    }
+})) // express middleware 
 app.use(express.static('public')); // app contains public files
 app.get('*', (req, res) => {
     const store = createStore();
