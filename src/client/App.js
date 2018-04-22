ff687208287b9d2e7e037d83afc0477d8f4963b0
Mike Cheong <michaelkcheong@gmail.com>
2018-04-22 10:45:10 -0700

    Set up header component

--- src/client/App.js
+++ src/client/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config'
+import Header from './components/Header'
 
 const App = ({ route }) => {
     return (
         <div>
-            <h1>I'm a header</h1>
+            <Header />
             <div>{renderRoutes(route.routes)}</div>
         </div>    
     );
